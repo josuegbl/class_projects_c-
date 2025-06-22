376166a8b04645ceb0ede397129d49e45bdd7d1e
Josué Gómez Blanco <josuegbl@users.noreply.github.com>
2025-06-22 13:46:30 +0200

    Add files via upload

---
+++ Lesson_01/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+float precioTomate = 3.0f;
+float precioPatata = 1.0f;
+float precioHuevos = 4.0f;
+float dinero = 10.0f;
+float nuevoDinero = 0.0f;
+float gasto = 0;
+int tomates = 1;
+int patatas = 1;
+int huevos = 1;
+
+gasto = tomates * precioTomate + patatas * precioPatata + huevos * precioHuevos;
+
+Console.WriteLine("My First script");
+if (gasto <= dinero)
+{
+    Console.WriteLine($"Te has gastado {gasto} euros");
+    nuevoDinero = dinero - gasto;
+    Console.Write($"Te quedan {nuevoDinero} euros");
+
+}
+else
+{
+    Console.Write("Not enough money");
+}
---
+++ Lesson_01/first_text.md
@@ -0,0 +1,33 @@
+SIENDO
+precioTomate = 3
+precioPatatas = 1
+precioHuevos = 4
+saldo = 10
+tomates = 1
+patatas = 1
+huevos = 1
+
+gasto = tomates * precioTomate + patatas * precioPatatas + huevos * precioHuevos
+
+SI gasto MENOR O IGUAL QUE saldo:
+	IMPRIMIR "You spent ", gasto, "euros"
+	nuevoSaldo = saldo - gasto
+	IMPRIMIR "You have " , nuevoSaldo, "euros to spend"
+SI NO:
+	IMPRIMIR "You have not enough money"
+
+	
+** SIENDO
+** SI -> ENTONCES
+** SI NO SI -> ENTONCES
+** SI X ES Y
+** O
+** Y
+** IMPRIMIR
+
+** MAYOR QUE 
+** MENOR QUE
+** MAYOR O IGUAL QUE
+** MENOR O IGUAL QUE
+** IGUAL QUE
+** DISTINTO QUE
