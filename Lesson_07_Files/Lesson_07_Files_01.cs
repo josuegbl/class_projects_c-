c778520a8b392f9ab37060c732b21081b1469f97
josuegbl <josuegbl@gmail.com>
2025-06-30 17:05:06 +0200

    kk

---
+++ Lesson_07_Files/Lesson_07_Files_01.cs
@@ -0,0 +1,109 @@
+﻿namespace Lesson_07_Files;
+
+public class Lesson_07_Files_01
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("En EjemploFicheros");
+        //string test = File.ReadAllText("testStringInput.txt");
+
+        //File.WriteAllText("testStringOutput.txt", test);
+
+        //Console.WriteLine(test);
+
+        //string[] testlines = File.ReadAllLines("testLinesInput.txt");
+
+        //foreach (string line in testlines)
+        //{
+        //    Console.WriteLine(line);
+        //}
+
+        //testlines[2] = testlines[2] + "PRUEBA";
+        //File.WriteAllLines("testLinesOutput.txt", testlines);
+
+        //File.AppendAllText("testStringOutput.txt", "PRUEBA APPEND");
+
+        //string[] moreWords =
+        //{
+        //    "en", "clase", "con", "gente"
+        //};
+
+        //File.AppendAllLines("testLinesOutput.txt", moreWords);
+
+        Console.WriteLine(readFileAsString());
+        readTenChains();
+        readNumbersFromFile();
+
+    }
+    /// Realizar los siguientes ejercicios de lectura de ficheros.
+    /// . Para casa ejercicio habrá que generar, al menos, una función
+    /// específica para alojar el codigo, que será llamada desde el Main.
+
+    /// Crear una funcion que no reciba ningun parametro y devuelva un entero.
+    /// . Leer un fichero de entrada como string y escribir su longitud en otro
+    /// de salida.
+    /// . Ademas, devolver como resultado de la función la longitud del string leido y 
+    /// sacar por consola dicha longitud (desde el Main).
+
+    public static int readFileAsString()
+    {
+        string text = File.ReadAllText("fileToReadfunc1.txt");
+        int lenghtString = text.Length;
+
+        //File.Create("fileToWritefunc1.txt").Dispose();
+        File.WriteAllText("fileToWritefunc1.txt", lenghtString.ToString());
+        return lenghtString;
+    }
+
+    /// Crear una funcion que no reciba ningun parametro y que no devuelva nada.
+    /// . La funcion leerá por lineas un fichero que contenga al menos 10 cadena de 
+    /// texto.
+    /// . La funcion generará tres numeros aleatorios, que se han de corresponder con los
+    /// posibles "indices" del fichero leído.
+    /// . Las posiciones indicadas por los números aleatorios se convertirán a mayúsculas.
+    /// . Una vez terminado el proceso, la función volverá a escribir por líneas en
+    /// el mismo fichero de entrada
+
+    public static void readTenChains()
+    {
+        string[] chainsFromFile = File.ReadAllLines("fileReadTenChains.txt");
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++)
+        {
+            int index = random.Next(0, chainsFromFile.Length);
+            chainsFromFile[index] = chainsFromFile[index].ToUpper();
+        }
+
+        File.AppendAllLines("fileReadTenChains.txt", chainsFromFile);
+    }
+
+    /// Crear una funcion que no reciba ningun parametro y no devuelva nada.
+    /// - Leer un fichero por lineas, que contenga los 10 primeros numeros naturales,
+    /// en orden, escritos por parabras (Cero, Uno....)
+    /// . Escribir un fichero por lineas. Si el equivalente entero de la palabra leída 
+    /// (Cero -> 0, Uno -> 1...) es par, poner la palabra en mayuscula.
+    /// Si el valor es impar, escribir el numero en el fichero tal que: 
+    /// "El numero NUMERO es impar".
+    public static void readNumbersFromFile()
+    {
+        string[] chainsFromFile = File.ReadAllLines("fileReadNumbersFromFile.txt");
+        string[] numbersAndComments = new string[chainsFromFile.Length];
+
+        for (int i = 0; i < chainsFromFile.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                numbersAndComments[i] = chainsFromFile[i].ToUpper(); 
+            }
+            else
+            {
+                numbersAndComments[i] = "El número " + i.ToString() + " es impar";
+            }
+        }
+        //File.Create("fileWriteNumbersFromFile.txt").Dispose();
+        File.WriteAllLines("fileWriteNumbersFromFile.txt", numbersAndComments);
+
+    }
+
+}
---
+++ Lesson_07_Files/Lesson_07_Files_02.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Lesson_07_Files;
+public class Lesson_07_Files_02
+{
+    public static void Main(string[] args)
+    {
+        //sumRandomNumbers(7);
+        //showWhenTrue();
+        //string[] wordsWichChar= findTheChar('h  ');
+        //foreach (string word in wordsWichChar) Console.WriteLine(word);
+        //string[] wordsRepeated = matchWords();
+        //foreach(string word in wordsRepeated) Console.WriteLine(word);
+        splitString();
+
+    }
+    //* Crear una función que reciba como parámetro un número entero y no devuelva nada.
+    //• La función generará tantos números aleatorios entre 1 y 10 como indique el número.
+    //• La función escribirá en un fichero por líneas esos números y, al final del fichero, su suma.
+    public static void sumRandomNumbers(int maxRandomRounds)
+    {
+        Random random = new Random();
+        string[] randomNumbersToFile = new string[maxRandomRounds+1];
+        int sum = 0;
+
+        for (int i = 0; i < maxRandomRounds; i++)
+        {
+            int value = random.Next(0, 11);
+            sum += value;
+            randomNumbersToFile[i] = value.ToString();
+        }
+        randomNumbersToFile[maxRandomRounds] = sum.ToString();
+        File.WriteAllLines("randomNumbersAndSum.txt", randomNumbersToFile);
+    }
+
+    //* Crear una función que no reciba ningún parámetro ni devuelva nada.
+    //﻿﻿• Leerá de dos ficheros: uno por líneas, que contendrá en cada línea
+    //  un valor booleano (true o false) true y false; otro que en cada línea
+    //  contenga una palabra palabras por líneas.
+    //﻿﻿• Irá recorriendo las palabras leídas y, si el equivalente
+    //del indice que ocupa esa palabra en los booleanos leídos es true,
+    //mostrará la palabra por consola.
+    public static void showWhenTrue()
+    {
+        string[] arraysOfBoolString = File.ReadAllLines("booleantToRead.txt");
+        string[] arraysOfwords = File.ReadAllLines("wordsToPrintIfTrue.txt");
+        for (int i = 0; i< arraysOfwords.Length; i++)
+        {
+            if (bool.Parse(arraysOfBoolString[i]))
+            {
+                Console.WriteLine(arraysOfwords[i]);
+            }
+        }
+    }
+
+    //* Crear una función que reciba una letra como parámetro y devuelva un array de string.
+    //• ﻿﻿La función leerá un fichero por líneas y buscará qué palabras contienen la letra
+    //pasada como parámetro, almacenándolas en un array.
+    //• ﻿﻿La función devolverá como resultado el array de palabras que contienen la
+    //letra pasada como parámetro.
+    //NOTA • No hace falta controlar "grano fino" el array devuelto como resultado
+    //(se pueden escribir líneas en blanco).
+    public static string[] findTheChar(char c)
+    {
+        string[] arraysOfwords = File.ReadAllLines("wordsToPrintIfTrue.txt");
+        string[] wordsWithChar = new string[arraysOfwords.Length];
+
+        for (int i = 0; i < arraysOfwords.Length; i++)
+        {
+            if (arraysOfwords[i].Contains(c))
+            {
+                wordsWithChar[i] = arraysOfwords[i];
+            }
+        }
+        return wordsWithChar;
+    }
+
+    /// Crear una funcion que no reciba ningun parametro y que devuelva un
+    /// Array de strings.
+    /// La funcion leerá por lineas dos ficheros que contienen cadenas de texto.
+    /// la funcion devolverá un array con aquellas parabras que estén, al menos
+    /// una vez, en un fichero y en el otro.
+    /// NOTA: No hace falta controlar "grano fino" el array devuelto como resultado 
+    /// (se pueden escribir lineas en blanco)
+    
+    public static string[] matchWords()
+    {
+        string[] arraysOfwords1 = File.ReadAllLines("wordsList1.txt");
+        string[] arraysOfwords2 = File.ReadAllLines("wordsList2.txt");
+        string[] wordsMatch = new string[arraysOfwords2.Length];
+        int counter = 0;
+        foreach (string wordFile1 in arraysOfwords1)
+        {
+            bool wordAdded = false;
+            foreach (string wordAppend in wordsMatch)
+            {
+                if (wordFile1 == wordAppend) wordAdded = true;
+            }
+            if (!wordAdded)
+            {
+                for (int i = 0; i < arraysOfwords2.Length; i++)
+                {
+                    if (wordFile1 == arraysOfwords2[i])
+                    {
+                        counter++;
+                        wordsMatch[counter] = wordFile1;
+                    }
+                }
+            }
+        }
+        return wordsMatch;
+
+    }
+
+    /// Crear una funcion que no reciba ningun parametro ni devuelva nada,.
+    /// La funcion leera un fichero como un string.
+    /// La funcion leerá otro fichero como lineas del que sacará  numeros
+    /// enteros positivos.
+    /// La funcion ira sacando substrings de la string leida en el fichero de 
+    /// entrada, cuya longitud vendrá indicada por los numeros leídos.
+    /// Cuando no haya numeros disponibles, o el siguiente substring no pueda 
+    /// crearse completo (esto es, se puede crear un substring de 6 caracteres
+    /// y "quedan" 3 en el string origen) la funcion escribirá esos strings por 
+    /// lineas en otro fichero de salida.
+
+    public static void splitString()
+    {
+        string text = File.ReadAllText("textToSplit.txt");
+        string[] lengthSubstrings = File.ReadAllLines("numberToSplit.txt");
+        string[] wordsLines = new string[lengthSubstrings.Length];
+        int startIndex = 0;
+
+        
+        for (int i = 0; i < lengthSubstrings.Length; i++)
+        {
+            int length = int.Parse(lengthSubstrings[i]);
+
+            if ((startIndex + length) <= text.Length)
+            {
+                wordsLines[i] = text.Substring(startIndex, length);
+                startIndex += length;
+            }
+
+        }
+        File.AppendAllLines("textSplitted.txt", wordsLines);
+
+    }
+}
